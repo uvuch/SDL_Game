88ee0e8a2a3599080f4c7b7b1fbec52c89cd8846
Serge Muzyka <siarhei.muzyka@pennmedicine.upenn.edu>
2023-06-13 17:42:31 -0400

    Organized game code by splitting it onto Init/EventHandling/Update/Render sections

--- SDL_Game/main.cpp
+++ SDL_Game/main.cpp
@@ -11,45 +11,57 @@
 
 bool g_bRunning = false;
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-SDL_RendererInfo rendererInfo;
+SDL_Window* g_pWindow = nullptr;
+SDL_Renderer* g_pRenderer = nullptr;
 
-int main(int argc, const char * argv[]) {
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        std::cout << "ERROR: SDL Library Initialization failed\n";
-        return -1;
-    }
-    
-    g_pWindow = SDL_CreateWindow("Chapter 1: Setting up SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+bool init(const char *title, int xpos, int ypos, int height, int width, int flags) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) return false;
+
+    g_pWindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
     if (!g_pWindow) {
-        std::cout << "ERROR: Cannot create window\n";
         SDL_Quit();
-        return -1;
+        return false;
     }
     
-    std::cout << "Window has been created successfully\n";
     g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
     if (!g_pRenderer) {
-        std::cout << "ERROR: Cannot create renderer\n";
         SDL_DestroyWindow(g_pWindow);
         SDL_Quit();
+        return false;
+    }
+    
+    return true;
+}
+
+void handleEvents() {
+    SDL_Event e;
+    while( SDL_PollEvent( &e ) )
+        if( e.type == SDL_QUIT ) g_bRunning = false;
+}
+
+void update() {
+    
+}
+
+void render() {
+    SDL_SetRenderDrawColor(g_pRenderer, 0, 255, 0, 50); // RGBA
+    SDL_RenderClear(g_pRenderer);
+    SDL_RenderPresent(g_pRenderer);
+}
+
+
+int main(int argc, const char * argv[]) {
+    if (!init("Chapter 1: Setting up SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN)) {
+        std::cout << "ERROR: Initialization step failed\n";
         return -1;
     }
-    std::cout << "Renderer has been created successfully\n";
-    
-    SDL_GetRendererInfo(g_pRenderer, &rendererInfo);
-    std::cout << "Renderer: " << rendererInfo.name << "\n";
-    
-    int res = SDL_SetRenderDrawColor(g_pRenderer, 0, 255, 0, 50); // RGBA
-    std::cout << "SetRenderDrawColor: " << res << "\n";
-    
-    res = SDL_RenderClear(g_pRenderer);
-    std::cout << "SDL_RenderClear: " << res << "\n";
-    
-    SDL_RenderPresent(g_pRenderer);
 
-    SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+    g_bRunning = true;
+    while( g_bRunning ) {
+        handleEvents();
+        update();
+        render();
+    }
     
     SDL_Quit();
     return 0;
